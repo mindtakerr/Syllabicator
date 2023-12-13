a9d3b4d6d71eb1bdecde470f2449fa0bcd7e4f7d
mindtakerr <michael@bakedhamgames.net>
2023-12-13 08:51:18 -0500

    Added the ability to do a song skeleton

--- Syllabicator/Controllers/SyllabicationController.cs
+++ Syllabicator/Controllers/SyllabicationController.cs
@@ -79,6 +79,102 @@ namespace Syllabicator.Controllers
             return Ok(new GlossaryItem() { Word = word, Syllables = new string[] { word } });
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="UltraStarFile"></param>
+        /// <param name="lang"></param>
+        /// <param name="beats"></param>
+        /// <returns></returns>
+        public IActionResult CreateSongSkeleton([FromBody] string UltraStarFile, [FromQuery] string lang, [FromQuery] int beats)
+        {
+            return Ok(SongSkeleton(UltraStarFile, lang, beats));
+        }
+
+        private List<string> SongSkeleton(string UltraStarFile, string lang, int beats)
+        {
+            List<string> Output = new List<string>();
+
+            var lines = UltraStarFile.Split('\n');
+            var Glossary = LoadGlossary(lang);
+
+            int SongCounter = 0;
+
+            foreach (var line in lines)
+            {
+                string tline = line.Trim();
+                var Words = line.Split(' ');
+                foreach (var W in Words)
+                {
+                    // Clean up the word
+                    string Word = W.Replace(",", string.Empty).Replace("\"", string.Empty).Replace("?", string.Empty).Replace("!", string.Empty).Replace(".", string.Empty).Replace(";", string.Empty);
+
+                    GlossaryItem Syllabicated = Glossary.SingleOrDefault(x => x.Word == Word.Trim().ToLower());
+                    if (Syllabicated == null)
+                    {
+                        if (lang == "English")
+                        {
+                            // Try to get the syllables from howmanysyllables.com
+                            string url = "https://www.howmanysyllables.com/syllables/" + Word;
+                            string html = new WebClient().DownloadString(url);
+                            string SyllableInfoCheckString = "into syllables: &nbsp; <span class=\"Answer_Red\" data-nosnippet>";
+                            Thread.Sleep(102); // Sleep so that we don't slam the how many syllables website
+
+                            if (html.Contains(SyllableInfoCheckString))
+                            {
+                                var parts = html.Replace(SyllableInfoCheckString, "☯").Split('☯');
+                                string syllables = parts[1].Split('<')[0];
+                                Syllabicated = new GlossaryItem
+                                {
+                                    Word = Word,
+                                    Syllables = syllables.Split('-')
+                                };
+
+                                // Can we add this to our glossary?
+                                //string p = jsonPath.Replace(".json", ".json.flat");
+                                //System.IO.File.WriteAllText(p, "Hello");
+                                Glossary.Add(Syllabicated);
+                                SaveGlossary(Glossary, lang);
+                            }
+                        }
+                    }
+
+                    // If, after all this, Syllabicated is still null... just make it a one-syllable word
+                    if (Syllabicated == null)
+                    {
+                        Syllabicated = new GlossaryItem() { Word = Word, Syllables = new string[] { Word } };
+                    }
+
+                    for (int i = 0; i < Syllabicated.Syllables.Length; i++)
+                    {
+                        var ThisSongLine = new SongLine();
+                        ThisSongLine.Starter = ":";
+                        ThisSongLine.StartingTime = SongCounter;
+                        ThisSongLine.NoteLength = beats;
+                        ThisSongLine.NoteValue = 12;
+                        ThisSongLine.Syllable = Syllabicated.Syllables[i];
+
+                        if (i == 0 && Word.Substring(0, 1).ToUpper() == Word.Substring(0, 1))
+                        {
+                            // Capitalize this syllable
+                            ThisSongLine.Syllable = ThisSongLine.Syllable.Substring(0, 1).ToUpper() + ThisSongLine.Syllable.Substring(1);
+                        }
+                        if (i == Syllabicated.Syllables.Length - 1)
+                            ThisSongLine.Syllable += " "; // Add a space at the end of the word
+                        Output.Add(ThisSongLine.ToString());
+                        SongCounter += beats + 1;
+                    }
+                }
+
+                SongCounter++;
+                Output.Add("- " + SongCounter);
+
+                SongCounter += beats * 2; // Add more beats for the line break?
+            }
+
+            return Output;
+        }
+
         private void CleanGlossary(string lang)
         {
             var Glossary = LoadGlossary(lang);
